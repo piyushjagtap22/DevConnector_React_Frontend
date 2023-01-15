fab646938aa452920062601d152e982c658ece86
piyushjagtap22 <jagspiyush@gmail.com>
2023-01-15 18:53:28 +0530

    updated baseurl

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import { LOGOUT } from '../actions/types';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://node-app-vercel.vercel.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
